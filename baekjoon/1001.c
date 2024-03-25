0ba9c5e53d54aed4cf9b8ec4893b05320f54d07e
peachwang122 <na012931286@gmail.com>
2024-03-25 20:34:09 +0900

    solve dkwlr

--- baekjoon/1001.c
+++ baekjoon/1001.c
@@ -2,5 +2,11 @@
 
 int main(void)
 {
-    printf("hello world");
+    printf("int 자료형의 크기: %d바이트\n", sizeof(int));
+    printf("float 자료형의 크기: %d바이트\n", sizeof(float));
+    printf("char 자료형의 크기: %d바이트\n", sizeof(char));
+    printf("double 자료형의 크기: %d바이트\n", sizeof(double));
 }
+
+//gcc 1001.c
+// ./a.out  으로 실행
